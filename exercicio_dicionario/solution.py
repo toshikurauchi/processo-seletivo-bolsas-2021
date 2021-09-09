535d0f0e2f691370d19f773179be31bd2451c452
Luiza Valezim Augusto Pinto <62949391+LuizaValezim@users.noreply.github.com>
2021-09-09 17:45:49 -0300

    Adicionando alguns testes para o ex da biblioteca

--- exercicio_dicionario/solution.py
+++ exercicio_dicionario/solution.py
@@ -1,9 +1,16 @@
 def primeiras_ocorrencias(palavra):
-    ocorrencias = {}
+    try: 
+        ocorrencias = {}
 
-    for i in range(len(palavra)):
-        caractere = palavra[i]
-        if caractere not in ocorrencias:
-            ocorrencias[caractere] = i
+        for i in range(len(palavra)):
+            caractere = palavra[i]
+            if caractere not in ocorrencias:
+                ocorrencias[caractere] = i
+                
+        return ocorrencias
 
-    return ocorrencias
+    except:
+        return None
+
+palavra = 214432423
+print(primeiras_ocorrencias(palavra))
--- exercicio_dicionario/test_solution.py
+++ exercicio_dicionario/test_solution.py
@@ -1,4 +1,23 @@
 from solution import primeiras_ocorrencias
+import pytest
 
 
-# Escreva seus testes neste arquivo. Para ajudar já importamos a função.
+# Testando solução com uma palavra muito longa
+def test_primeiras_ocorrencias_palavra_grande():
+    palavra = 'pneumoultramicroscopicossilicovulcanoconiótico'
+    assert primeiras_ocorrencias(palavra) == {'p': 0, 'n': 1, 'e': 2, 'u': 3, 'm': 4, 'o': 5, 'l': 7, 't': 8, 'r': 9, 'a': 10, 'i': 12, 'c': 13, 's': 16, 'v': 30, 'ó': 41}
+
+# Testando solução com uma frase aleatória
+def test_primeiras_ocorrencias_frase():
+    palavra = 'olarmaeu  ahoi'
+    assert primeiras_ocorrencias(palavra) == {'o': 0, 'l': 1, 'a': 2, 'r': 3, 'm': 4, 'e': 6, 'u': 7, ' ': 8, 'h': 11, 'i': 13}
+
+# Testando solução com números
+def test_primeiras_ocorrencias_numeros():
+    palavra = 214432423
+    assert primeiras_ocorrencias(palavra) == None, "O formato do argumento para função é um inteiro e não uma string"
+
+# Testando solução nula
+def test_primeiras_ocorrencias_nulo():
+    palavra = ""
+    assert primeiras_ocorrencias(palavra) == {}, "O valor inserido está sendo reconhecido como nulo"
