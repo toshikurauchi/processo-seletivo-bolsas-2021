79a143cca34d19c7d60f8e2ddf9837793fb2f4da
DiegoSaragozaSilva <diegosaragozaa2@gmail.com>
2021-09-09 18:33:53 -0300

    Primeira iteração

--- exercicio_dicionario/test_solution.py
+++ exercicio_dicionario/test_solution.py
@@ -1,4 +1,93 @@
 from solution import primeiras_ocorrencias
+import pytest
 
+def test_answer():
 
-# Escreva seus testes neste arquivo. Para ajudar já importamos a função.
+    # Casos -> Dicionário que guarda os testes e suas informações.
+    # A estrutura foi pensada para que a adição e remoção de testes seja rápida e não exija trabalho.
+    # Existem três casos pensados para os testes:
+    #   equals --> Caso em que o teste é feito para garantir que o retorno da função seja igual do desejado.
+    #   Exige: 
+    #       arg ---> Argumento a ser passado para a função.
+    #       ans ---> Resposta esperada do retorno da função.
+    #
+    #   nequals --> Caso em que o teste é feito para garantir que o retorno da função seja diferente do desejado.
+    #   Exige: 
+    #       arg ---> Argumento a ser passado para a função.
+    #       ans ---> Resposta a ser comparada ao retorno da função.
+    #
+    #   exception --> Caso em que o teste é feito e espera-se que ele reaja com uma exceção.
+    #   Exige: 
+    #       arg ---> Argumento a ser passado para a função.
+    #       exc-type ---> Tipo da exceção esperada da função.
+
+    casos = {
+        1: {
+            'type': 'equals',
+            'arg': 'abracadabra',
+            'ans': {'a': 0, 'b': 1, 'r': 2, 'c': 4, 'd': 6}
+        },
+        2: {
+            'type': 'equals',
+            'arg': 'AaAaBbCdDe',
+            'ans': {'A': 0, 'a': 1, 'B': 4, 'b': 5, 'C': 6, 'd': 7, 'D': 8, 'e': 9}
+        },
+        3: {
+            'type': 'equals',
+            'arg': '.!@#$)(%$#$',
+            'ans': {'.': 0, '!': 1, '@': 2, '#': 3, '$': 4, ')': 5, '(': 6, '%': 7}
+        },
+        4: {
+            'type': 'equals',
+            'arg': 'certa vez, eu vi algo de muito longe.',
+            'ans': {'c': 0, 'e': 1, 'r': 2, 't': 3, 'a': 4, ' ': 5, 'v': 6, 'z': 8, ',': 9,
+                    'u': 12, 'i': 15, 'l': 18, 'g': 19, 'o': 20, 'd': 22, 'm': 25, 'n': 33,
+                     '.': 36}
+        },
+        5: {
+            'type': 'nequals',
+            'arg': 'abracadabra',
+            'ans': {'a': 3, 'b': 1, 'r': 2, 'c': 4, 'd': 6}
+        },
+        6: { # Caso onde os indices estao sendo contados com um numero a mais.
+            'type': 'nequals',
+            'arg': 'AaAaBbCdDe',
+            'ans': {'A': 1, 'a': 2, 'B': 5, 'b': 6, 'C': 7, 'd': 8, 'D': 9, 'e': 10}
+        },
+        7: {
+            'type': 'nequals',
+            'arg': 'certa vez, eu vi algo de muito longe.',
+            'ans': {'c': 0, 'e': 1, 'r': 2, 't': 3, 'a': 4, ' ': 26, 'v': 6, 'z': 8, ',': 9,
+                    'u': 12, 'i': 15, 'l': 18, 'g': 19, 'o': 20, 'd': 22, 'm': 25, 'n': 33,
+                    '.': 36}
+        },
+        8: { # Caso onde um inteiro e passado, esperace uma excecao do tipo TypeError
+            'type': 'exception',
+            'arg': 128175189259043,
+            'exc-type': TypeError
+        },
+        9: { # Caso onde uma lista e passado, esperace um tratamento sobre? Partirei do pressuposto que não
+            'type': 'equals',
+            'arg': [1, 2, 3, 3, 2, 1],
+            'ans': {1: 0, 2: 1, 3: 2}
+        },
+        10: { # Caso simples onde a string de argumento e vazia
+            'type': 'equals',
+            'arg': '',
+            'ans': {}
+        },
+        11: { # Caso similar a lista, porem com uma tupla
+            'type': 'equals',
+            'arg': (1, 2, 3, 3, 2, 1),
+            'ans': {1: 0, 2: 1, 3: 2}
+        }
+    }
+
+    # Estrutura que automaticamente faz os testes de acordo com os dados dos casos
+    for i in range(1, len(casos)):
+        if casos[i]['type'] == 'equals':
+            assert (primeiras_ocorrencias(casos[i]['arg']) == casos[i]['ans'])
+        elif casos[i]['type'] == 'nequals':
+            assert (primeiras_ocorrencias(casos[i]['arg']) != casos[i]['ans'])
+        elif casos[i]['type'] == 'exception':
+            pytest.raises(casos[i]['exc-type'], primeiras_ocorrencias, casos[i]['arg'])
