48055326b16bbbb548c93253e21c4ff02c6aae5f
camilamagni <camilinhamagni@gmail.com>
2021-09-09 19:29:09 -0300

    testes iniciais

--- exercicio_dicionario/solution.py
+++ exercicio_dicionario/solution.py
@@ -3,7 +3,15 @@ def primeiras_ocorrencias(palavra):
 
     for i in range(len(palavra)):
         caractere = palavra[i]
+        
         if caractere not in ocorrencias:
             ocorrencias[caractere] = i
-
+        # ocorrencias[caractere] = i
     return ocorrencias
+
+
+print(primeiras_ocorrencias('abracadabra'))
+print(primeiras_ocorrencias('y'))
+print(primeiras_ocorrencias('testando mais de uma palavra'))
+print(primeiras_ocorrencias('ola, mundo!'))
+print(primeiras_ocorrencias('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaabgtfr'))
