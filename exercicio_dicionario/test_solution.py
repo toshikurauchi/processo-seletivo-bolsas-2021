b8eff6b3e910f21e1d1b04f001ba3b9148cef30e
Guilherme Rameh <guilhermedr@al.insper.edu.br>
2021-09-10 16:17:23 -0300

    Corrigido nomes das funções para explicitamente dizerem o que testam e corrigido a mensagem de erro com dicas

--- exercicio_dicionario/test_solution.py
+++ exercicio_dicionario/test_solution.py
@@ -1,17 +1,15 @@
 from solution import primeiras_ocorrencias
 
 try:
-    def teste0():
+    def teste_string_vazia():
         # Verifica a função para strings vazias
 
         string = ""
         expected = {}
         teste = primeiras_ocorrencias(string)
-        print("\nDica:")
-        print("Sua função não parece funcionar para strings vazias\n")
-        assert teste==expected
+        assert teste==expected, "\nDica: Sua função não parece funcionar para strings vazias\n"
 
-    def teste1():
+    def teste_string_inteira():
         # Verifica se a função, com sucesso, determina o índice correto para a última letra, se 
         # essa for sua primeira ocorrência. O Erro comum aqui é talvez o código do aluno não esteja percorrendo
         # a string inteira.
@@ -19,50 +17,40 @@ try:
         string = "aaab"
         expected = {"a": 0, "b":len(string)-1}
         teste = primeiras_ocorrencias(string)
-        print("\nDica:")
-        print("Verifique se sua função percorre toda a string!\n")
-        assert teste==expected
+        assert teste==expected, "\nDica: Verifique se sua função percorre toda a string!\n"
 
-    def teste2():
+    def teste_override_primeira():
         # Verifica se a função separa com sucesso a primeira ocorrência da letra,
         # sem trocar com a última ocorrência, erro que pode ser comum
 
         string = "abba"
         expected = {"a": 0, "b":1}
         teste = primeiras_ocorrencias(string)
-        print("\nDica:")
-        print("Cuidado! Parece que sua função está selecionando as últimas ocorrências, e não as primeiras.\n")
-        assert teste==expected
+        assert teste==expected, "\nDica: Cuidado! Parece que sua função está selecionando as últimas ocorrências, e não as primeiras.\n"
 
-    def teste3():
+    def teste_string_pequena():
         # Verifica função para valores bem pequenos (uma string com apenas 1 caractere)
 
         string = "a"
         expected = {"a": 0}
         teste = primeiras_ocorrencias(string)
-        print("\nDica:")
-        print("Sua função parece estar falhando com entradas bem pequenas. Reveja seu código e tente novamente\n")
-        assert teste==expected
+        assert teste==expected, "\nDica: Sua função parece estar falhando com entradas bem pequenas. Reveja seu código e tente novamente\n"
 
-    def teste4():
+    def teste_string_grande():
         # Verifica função para valores bem grandes
 
         string = "abababababababababababababababababababcd"
         expected = {"a": 0, "b": 1, "c": len(string)-2, "d": len(string)-1}
         teste = primeiras_ocorrencias(string)
-        print("\nDica:")
-        print("Sua função parece estar falhando com entradas bem grandes. Reveja seu código e tente novamente\n")
-        assert teste==expected
+        assert teste==expected, "\nDica: Sua função parece estar falhando com entradas bem grandes. Reveja seu código e tente novamente\n"
 
-    def teste5():
+    def teste_string_espaco():
         # Verifica função para strings com espaços
 
         string = "O cachorro"
         expected = {"O": 0, " ": 1, "c": 2, "a": 3, "h": 5, "o": 6, "r": 7}
         teste = primeiras_ocorrencias(string)
-        print("\nDica:")
-        print("Preste atenção em strings com espaços. Espaços também contam como caracteres\n")
-        assert teste==expected
+        assert teste==expected, "\nDica: Preste atenção em strings com espaços. Espaços também contam como caracteres\n"
 
 except Exception as error:
     print(f"Ops! Ocorreu um erro: \n{error}")
