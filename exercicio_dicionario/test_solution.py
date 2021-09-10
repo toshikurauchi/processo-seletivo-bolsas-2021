226a16cf64771908bdc8be6f1299953720b32908
carlosinaacio <carlosai@al.insper.edu.br>
2021-09-09 21:00:09 -0300

    Dicionários

--- exercicio_dicionario/test_solution.py
+++ exercicio_dicionario/test_solution.py
@@ -1,4 +1,41 @@
 from solution import primeiras_ocorrencias
 
+def test_dict_keys(entrada):
+    # verificar se as chaves retornadas pela função são válidas
 
-# Escreva seus testes neste arquivo. Para ajudar já importamos a função.
+    response = primeiras_ocorrencias(entrada)
+    for key in response.keys():
+        assert len(key) == 1                            # verifica se a chave não é um único carácter
+        assert key.isalpha()                            # verifica se é uma letra
+
+def test_dict_values(entrada):
+    # verifica se os valores são válidos
+    
+    response = primeiras_ocorrencias(entrada)
+    for value in response.values():
+        assert type(value) == int or type(value) == str
+
+        if type(value) == str:                          # caso o retorno seja em string
+            assert value.isnumeric()                    # verifica se só recebemos números
+            assert len(value) < 3 and len(value) > 0    # verifica se o tamanho do retorno é razoável
+
+def solution(palavra):
+    ocorrencias = {}
+
+    for i in range(len(palavra)):
+        caractere = palavra[i]
+        if caractere not in ocorrencias:
+            ocorrencias[caractere] = i
+
+    return ocorrencias
+
+def test_correct_answer(entrada):
+    # verificamos se a resposta é a esperada
+
+    assert primeiras_ocorrencias(entrada) == solution(entrada)
+
+entrada = 'abracadabra'
+
+test_dict_keys(entrada)        # validamos as chaves
+test_dict_values(entrada)      # validamos os valores
+test_correct_answer(entrada)   # validamos a resposta
