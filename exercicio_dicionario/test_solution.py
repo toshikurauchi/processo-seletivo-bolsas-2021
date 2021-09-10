17e1eba7c84e2e8602abdde51db58864e5837544
carlosinaacio <carlosai@al.insper.edu.br>
2021-09-09 21:14:48 -0300

    Pequena correção na nomeação de variaveis

--- exercicio_dicionario/test_solution.py
+++ exercicio_dicionario/test_solution.py
@@ -1,19 +1,19 @@
 from solution import primeiras_ocorrencias
 
-def test_dict_keys(entrada):
+def test_dict_keys(input):
     # verificar se as chaves retornadas pela função são válidas
 
-    response = primeiras_ocorrencias(entrada)
+    response = primeiras_ocorrencias(input)
     assert type(response) == dict                       # verifica se o retorno é um dicionário
 
     for key in response.keys():
         assert len(key) == 1                            # verifica se a chave não é um único carácter
         assert key.isalpha()                            # verifica se é uma letra
 
-def test_dict_values(entrada):
+def test_dict_values(input):
     # verifica se os valores são válidos
     
-    response = primeiras_ocorrencias(entrada)           # não precisamos verificar se o retorno é um dicionário aqui, pois já o fizemos em uma chamada anterior
+    response = primeiras_ocorrencias(input)           # não precisamos verificar se o retorno é um dicionário aqui, pois já o fizemos em uma chamada anterior
 
     for value in response.values():
         assert type(value) == int or type(value) == str 
@@ -32,13 +32,13 @@ def solution(palavra):
 
     return ocorrencias
 
-def test_correct_answer(entrada):
+def test_correct_answer(input):
     # verificamos se a resposta é a esperada
 
-    assert primeiras_ocorrencias(entrada) == solution(entrada)
+    assert primeiras_ocorrencias(input) == solution(input)
 
-entrada = 'abracadabra'
+input = 'abracadabra'
 
-test_dict_keys(entrada)        # validamos as chaves
-test_dict_values(entrada)      # validamos os valores
-test_correct_answer(entrada)   # validamos a resposta
+test_dict_keys(input)        # validamos as chaves
+test_dict_values(input)      # validamos os valores
+test_correct_answer(input)   # validamos a resposta
