0845efd6d77fee2f216e6756a0487431bebab2b4
LinkolnR <lincolninsper@gmail.com>
2021-09-10 18:21:31 -0300

    Removendo os tests com entradas invalidas

--- exercicio_dicionario/test_solution1.py
+++ exercicio_dicionario/test_solution1.py
@@ -10,9 +10,6 @@ from solution import primeiras_ocorrencias
 @pytest.mark.parametrize("palavra , dicionario",
 [("palavra",{"p":0,"a":1,"l":2,"v":4,"r":5}),
  ("PaLAVra",{"P":0,"a":1,"L":2,"A":3,"V":4,"r":5}),
- (31458 , {}),
- ([1,2,3,4,5],{}),
- (["banana","aquele","teste"],"error"),
  ("compartimentalização",{'c': 0, 'o': 1, 'm': 2, 'p': 3, 'a': 4, 'r': 5, 't': 6, 'i': 7, 'e': 9, 'n': 10, 'l': 13, 'z': 15, 'ç': 17, 'ã': 18} ), 
  ("AaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaABBBBBBBBBBBBBbbbb122222222222222222222333000000000000000000pP",{'A': 0, 'a': 1, 'B': 56, 'b': 69, '1': 73, '2': 74, '3': 94, '0': 97, 'p': 115, 'P': 116})
 
@@ -21,8 +18,6 @@ from solution import primeiras_ocorrencias
 def test_func(palavra,dicionario):
     if type(palavra) == str:
         assert primeiras_ocorrencias(palavra) == dicionario
-    else:
-        TypeError ("A entrada não foi uma string")
 
 #Teste com palavras Maiores
 def test_palavras_maiores():
