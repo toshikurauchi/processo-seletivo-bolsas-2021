d72628efe79f4ca4278e5013cc71e9cb812b2bba
Guilherme Rameh <guilhermedr@al.insper.edu.br>
2021-09-10 16:19:40 -0300

    Corrigido nomes das funções para explicitamente dizerem o que testam e corrigido a mensagem de erro com dicas

--- exercicio_dicionario/test_solution.py
+++ exercicio_dicionario/test_solution.py
@@ -7,7 +7,7 @@ try:
         string = ""
         expected = {}
         teste = primeiras_ocorrencias(string)
-        assert teste==expected, "\nDica: Sua função não parece funcionar para strings vazias\n"
+        assert teste==expected, "Dica: Sua função não parece funcionar para strings vazias."
 
     def teste_string_inteira():
         # Verifica se a função, com sucesso, determina o índice correto para a última letra, se 
@@ -17,7 +17,7 @@ try:
         string = "aaab"
         expected = {"a": 0, "b":len(string)-1}
         teste = primeiras_ocorrencias(string)
-        assert teste==expected, "\nDica: Verifique se sua função percorre toda a string!\n"
+        assert teste==expected, "Dica: Verifique se sua função percorre toda a string!"
 
     def teste_override_primeira():
         # Verifica se a função separa com sucesso a primeira ocorrência da letra,
@@ -26,7 +26,7 @@ try:
         string = "abba"
         expected = {"a": 0, "b":1}
         teste = primeiras_ocorrencias(string)
-        assert teste==expected, "\nDica: Cuidado! Parece que sua função está selecionando as últimas ocorrências, e não as primeiras.\n"
+        assert teste==expected, "Dica: Cuidado! Parece que sua função está selecionando as últimas ocorrências, e não as primeiras."
 
     def teste_string_pequena():
         # Verifica função para valores bem pequenos (uma string com apenas 1 caractere)
@@ -34,7 +34,7 @@ try:
         string = "a"
         expected = {"a": 0}
         teste = primeiras_ocorrencias(string)
-        assert teste==expected, "\nDica: Sua função parece estar falhando com entradas bem pequenas. Reveja seu código e tente novamente\n"
+        assert teste==expected, "Dica: Sua função parece estar falhando com entradas bem pequenas. Reveja seu código e tente novamente."
 
     def teste_string_grande():
         # Verifica função para valores bem grandes
@@ -42,7 +42,7 @@ try:
         string = "abababababababababababababababababababcd"
         expected = {"a": 0, "b": 1, "c": len(string)-2, "d": len(string)-1}
         teste = primeiras_ocorrencias(string)
-        assert teste==expected, "\nDica: Sua função parece estar falhando com entradas bem grandes. Reveja seu código e tente novamente\n"
+        assert teste==expected, "Dica: Sua função parece estar falhando com entradas bem grandes. Reveja seu código e tente novamente."
 
     def teste_string_espaco():
         # Verifica função para strings com espaços
@@ -50,7 +50,7 @@ try:
         string = "O cachorro"
         expected = {"O": 0, " ": 1, "c": 2, "a": 3, "h": 5, "o": 6, "r": 7}
         teste = primeiras_ocorrencias(string)
-        assert teste==expected, "\nDica: Preste atenção em strings com espaços. Espaços também contam como caracteres\n"
+        assert teste==expected, "Dica: Preste atenção em strings com espaços. Espaços também contam como caracteres."
 
 except Exception as error:
     print(f"Ops! Ocorreu um erro: \n{error}")
