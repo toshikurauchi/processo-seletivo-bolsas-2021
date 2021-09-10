7b2cfdb60c8a053b5a96eb36118d0a50a996408e
Your Name <thiagohpr@al.insper.edu.br>
2021-09-10 11:22:08 -0300

    Corrigindo testes e adicioinando um para entradas vazias

--- exercicio_dicionario/test_solution.py
+++ exercicio_dicionario/test_solution.py
@@ -1,65 +1,44 @@
 from solution import primeiras_ocorrencias
 
 class TestClass:
-    def test_type(self):
-        x="Hello World"
-        dic=primeiras_ocorrencias(x)
-        typ=isinstance(dic,dict)
-        assert typ==True
-
-    def test_valuesTypes(self):
+    def testTypes(self):
         x = "quantidade"
-        dic=primeiras_ocorrencias(x)
-        inteiro=True
+        dic = primeiras_ocorrencias(x)
+        assert isinstance(dic, dict)
         for v in dic.values():
-            if not isinstance(v,int):
-                inteiro=False
-                break
-        assert inteiro==True
+            assert isinstance(v, int)
 
-    def test_ascendingValues(self):
-        x = "abcdefghijklmnooooonm"
-        dic=primeiras_ocorrencias(x)
-        assert list(dic.values())==sorted(dic.values())
+    def testBlankEntry(self):
+        x=""
+        dic = primeiras_ocorrencias(x)
+        sol={}
+        assert dic==sol
 
-    def test_uniqueKeys(self):
-        x = "bananananananas"
-        dic=primeiras_ocorrencias(x)
-        unico=True
-        keys=[]
-        for k in dic.keys():
-            if k in keys:
-                unico=False
-                break
-            else:
-                keys.append(k)
-        assert unico==True
-
-    def test_initial(self):
+    def testInitial(self):
         x = "abcabcd"
         dic=primeiras_ocorrencias(x)
         sol={'a': 0, 'b': 1, 'c': 2, 'd': 6}
         assert dic==sol
     
-    def test_lowerWord(self):
+    def testLowerWord(self):
         x = "batata"
         dic=primeiras_ocorrencias(x)
         sol={'b': 0, 'a': 1, 't': 2}
         assert dic==sol
 
-    def test_higherWord(self):
+    def testHigherWord(self):
         x = "aBóbOrA"
         dic=primeiras_ocorrencias(x)
         sol={'a': 0, 'B': 1, 'ó': 2, 'b': 3, 'O': 4, 'r': 5, 'A': 6}
         assert dic==sol
 
-    def test_specialChars(self):
+    def testSpecialChars(self):
         x = "$$@@macarrãoÀ@@$$"
         dic=primeiras_ocorrencias(x)
         sol={'$': 0, '@': 2, 'm': 4, 'a': 5, 'c': 6, 'r': 8, 'ã': 10, 'o': 11, 'À': 12}
         assert dic==sol
         
-    def test_numericEntry(self):
+    def testNumericEntry(self):
         x = "01233210"
         dic=primeiras_ocorrencias(x)
         sol={'0': 0, '1': 1, '2': 2, '3': 3}
