5d3b7bbdc4b312b58f95885a6da64643c5c6ebce
Cleiltonss <90421421+Cleiltonss@users.noreply.github.com>
2021-09-23 20:09:25 -0300

    Update solution.py

--- exercicio_listas/solution.py
+++ exercicio_listas/solution.py
@@ -6,3 +6,14 @@ def subtracao_de_listas(lista1, lista2):
             resposta.append(elemento)
 
     return resposta
+#verifica lista_1 e lista_2
+lista_1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+lista_2 = [0, 2, 4, 6, 8]
+lista_3 = [1, 3, 5, 7, 9]
+
+def test_subtracao_de_listas():
+    assert lista_3 == subtracao_de_listas(lista_1, lista_2)
+#verifica lis
+lista_Maiusculo = ['A', 'B', 'C', 'D', 'E']
+lista_letras = ['A', 'b', 'c', 'D', 'E']
+lista_letras_novas = ['b', 'c']
