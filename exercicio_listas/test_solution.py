d05ee63b3be828e1eb350a4235609755cec254ee
Gian2608 <giancarlovr@al.insper.edu.br>
2021-09-25 16:33:07 -0300

    Testes utilizando parametrize

--- exercicio_listas/test_solution.py
+++ exercicio_listas/test_solution.py
@@ -1,35 +1,41 @@
 import pytest
 from solution import subtracao_de_listas
 
-@pytest.fixture
-def listas():
-    lista1=[1,2,3,23,44,"a","b","baaa",9.8]
-    lista2=[1,3,22,4,"ab","b","da",9.8]
-    return lista1,lista2
+#Entradas
+lista1=[1,2,3,23,44,"a","b","baaa",9.8]
+lista2=[1,3,22,4,"ab","b","da",9.8]
+lista3=["1",2,"3",23,44,"a","b","baaa",9.8]
+lista4=[1,3,22,4,"ab","b","da",9.8]
+
+#Saídas
+saida1=[2,23,44,"a","baaa"]
+saida2=[22,4,"ab","da"]
+saida3=["1",2,"3",23,44,"a","baaa"]
+saida4=[1,3,22,4,"ab","da"]
+
+lista_vazia=[]
 
 #Testes com listas diferentes
-def test_subtracao_de_lista_1(listas):
-    assert subtracao_de_listas(listas[0],listas[1])==[2,23,44,"a","baaa"]
-
-def test_subtracao_de_lista_2(listas):
-    assert subtracao_de_listas(listas[1],listas[0])==[22,4,"ab","da"]
+@pytest.mark.parametrize("entrada,saida",[([lista1,lista2],[saida1,saida2])])
+def test_listas_diferentes(entrada,saida):
+    assert subtracao_de_listas(entrada[0],entrada[1])==saida[0]
+    assert subtracao_de_listas(entrada[1],entrada[0])==saida[1]
 
 #Testes com lista vazia
-def test_subtracao_de_lista_vazia(listas):
-    assert subtracao_de_listas([],listas[0])==[]
+@pytest.mark.parametrize("entrada,saida",[([lista1,lista_vazia],[lista1,lista_vazia])])
+def test_lista_vazia(entrada,saida):
+    assert subtracao_de_listas(entrada[0],entrada[1])==saida[0]
+    assert subtracao_de_listas(entrada[1],entrada[0])==saida[1]
+    assert subtracao_de_listas(entrada[1],entrada[1])==saida[1]
 
-def test_subtracao_de_lista_vazia_2(listas):
-    assert subtracao_de_listas(listas[0],[])==listas[0]
+#Teste com listas iguais
+@pytest.mark.parametrize("entrada,saida",[([lista1,lista2],lista_vazia)])
+def test_lista_igual(entrada,saida):
+    assert subtracao_de_listas(entrada[0],entrada[0])==saida
+    assert subtracao_de_listas(entrada[1],entrada[1])==saida
 
-def test_subtracao_de_lista_vazia_2(listas):
-    assert subtracao_de_listas([],[])==[]
-
-#Teste com lista igual
-def test_subtracao_de_lista_igual(listas):
-    assert subtracao_de_listas(listas[0],listas[0])==[]
-
-#Teste com tipos diferentes
-def test_subtracao_de_lista_string_int():
-    lista1=["1",2,"3",23,44,"a","b","baaa",9.8]
-    lista2=[1,3,22,4,"ab","b","da",9.8]
-    assert subtracao_de_listas(lista1,lista2)==["1",2,"3",23,44,"a","baaa"]
+#Testes com tipos diferentes
+@pytest.mark.parametrize("entrada,saida",[([lista3,lista4],[saida3,saida4])])
+def test_lista_int_string(entrada,saida):
+    assert subtracao_de_listas(entrada[0],entrada[1])==saida[0]
+    assert subtracao_de_listas(entrada[1],entrada[0])==saida[1]
