8356b61dcdf4f02acc969c0aeb33caf2ace1d0ab
Cleiltonss <cleilton.mecatronico@gmail.com>
2021-09-28 17:25:58 -0300

    Mudanças no código exercicio_listas

--- exercicio_dicionario/test_solution.py
+++ exercicio_dicionario/test_solution.py
@@ -1,4 +1,2 @@
 from solution import primeiras_ocorrencias
 
-
-# Escreva seus testes neste arquivo. Para ajudar já importamos a função.
--- exercicio_listas/test_solution.py
+++ exercicio_listas/test_solution.py
@@ -5,51 +5,51 @@ import pytest
 
 #verifica lista_numeros_impares
 
-@pytest.mark.parametrize("test_input, expected", [([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [0, 2, 4, 6, 8]), ([1, 3, 5, 7, 9])])
+@pytest.mark.parametrize("lista_numeros, lista_pares, lista_impares", [([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 2, 4, 6, 8], [1, 3, 5, 7, 9])])
 
-def test_subtracao_de_listas(test_input, expected):
-    assert subtracao_de_listas(test_input) == expected
+def test_subtracao_de_listas(lista_numeros, lista_pares, lista_impares):
+    assert subtracao_de_listas(lista_numeros, lista_pares) == lista_impares
 
 #verifica lista_Maiuscula e lista_letras
                                                   
-@pytest.mark.parametrize("test_input, expected", [(['A', 'B', 'C', 'D', 'E'], ['A', 'b', 'c', 'D', 'E']), (['B', 'C'])])
+@pytest.mark.parametrize("lista_Maiuscula, lista_letras, lista_letras_final", [(['A', 'B', 'C', 'D', 'E'], ['A', 'b', 'c', 'D', 'E'], ['B', 'C'])])
 
-def test_subtracao_de_listas(test_input, expected):
-    assert subtracao_de_listas(test_input) == expected
+def test_subtracao_de_listas(lista_Maiuscula, lista_letras, lista_letras_final):
+    assert subtracao_de_listas(lista_Maiuscula, lista_letras) == lista_letras_final
                          
-#verifica lista_anime e lista_fã
+# #verifica lista_anime e lista_fã
 
-@pytest.mark.parametrize("test_input, expected", [([''Naruto', 'Dragon Ball', 'Bleach', 'Jujutsu Kaisen', 'Kimetsu no Aiba'], ['Naruto', 'Dragon Ball', 'Jujutsu Kaisen']), (['bleach', 'kimetsu no aiba'])])
+@pytest.mark.parametrize("lista_anime, lista_fã, lista_anime_final", [(['Naruto', 'Dragon Ball', 'Bleach', 'Jujutsu Kaisen', 'Kimetsu no Aiba'], ['Naruto', 'Dragon Ball', 'Jujutsu Kaisen'], ['Bleach', 'Kimetsu no Aiba'])])
 
-def test_subtracao_de_listas(test_input, expected):
-    assert subtracao_de_listas(test_input) == expected
+def test_subtracao_de_listas(lista_anime, lista_fã, lista_anime_final):
+    assert subtracao_de_listas(lista_anime, lista_fã) == lista_anime_final
 
-#verifica lista_nome e lista_nome_maior
+# #verifica lista2_nome e lista_nome_maior
 
-@pytest.mark.parametrize("test_input, expected", [(['Quézia Moura', 'Kevin Shinohara', 'Cleilton Sousa'], ['Kevin Shinohara', 'Cleilton Sousa']), (['Quezia Moura'])])
+@pytest.mark.parametrize("lista2_nome, lista_nome_maior, lista_maior_final", [(['Quézia Moura', 'Kevin Shinohara', 'Cleilton Sousa'], ['Kevin Shinohara', 'Cleilton Sousa'], ['Quezia Moura'])])
 
-def test_subtracao_de_listas(test_input, expected):
-    assert subtracao_de_listas(test_input) == expected
+def test_subtracao_de_listas(lista2_nome, lista_nome_maior, lista_maior_final):
+    assert subtracao_de_listas(lista2_nome, lista_nome_maior) == lista_maior_final
 
-#verifica lista_name e lista_name_maior
+# #verifica lista_name e lista_name_maior
 
-@pytest.mark.parametrize("test_input, expected", [(['Quézia Moura', 'Kevin Shinohara', 'Cleilton Sousa'], ['Kevin Shinohara', 'Cleilton Sousa']), (['QuéziaMoura'])])                                                    
+@pytest.mark.parametrize("lista_name, lista_name_maior, lista_name_final", [(['QuéziaMoura', 'KevinShinohara', 'CleiltonSousa'], ['KevinShinohara', 'CleiltonSousa'], ['QuéziaMoura'])])                                                    
 
-def test_subtracao_de_listas(test_input, expected):
-    assert subtracao_de_listas(test_input) == expected
+def test_subtracao_de_listas(lista_name, lista_name_maior, lista_name_final):
+    assert subtracao_de_listas(lista_name, lista_name_maior) == lista_name_final
 
-#verifica lista_variaveis e lista_variaveis_variadas
+# #verifica lista_variaveis e lista_variaveis_variadas
 
-@pytest.mark.parametrize("test_input, expected", [(['2', '4'], ['1', 2, '3', 4]), ([2, 4])]) 
+@pytest.mark.parametrize("lista_variaveis, lista_variaveis_variadas, lista_variaveis_final", [(['2', '4'], ['1', 2, '3', 4], [2, 4])]) 
 
-def test_subtracao_de_listas(test_input, expected):
-    assert subtracao_de_listas(test_input) == expected
+def test_subtracao_de_listas(lista_variaveis, lista_variaveis_variadas, lista_variaveis_final):
+    assert subtracao_de_listas(lista_variaveis, lista_variaveis_variadas) == lista_variaveis_final
     
-#verifica lista_numeros_inteiros e lista_numeros_reais
+# #verifica lista_numeros_inteiros e lista_numeros_reais
 
-@pytest.mark.parametrize("test_input, expected", [([1, 2, 3, 4, 5], [1.0, 2.0, 3.0, 4.0]), ([5])]) 
+@pytest.mark.parametrize("lista_numeros_inteiros, lista_numeros_reais, lista_reais_final", [([1.0, 2.0, 3.0, 4.0, 5], [1.0, 2.0, 3.0, 4.0], [5])]) 
 
-def test_subtracao_de_listas(test_input, expected):
-    assert subtracao_de_listas(test_input) == expected
+def test_subtracao_de_listas(lista_numeros_inteiros, lista_numeros_reais, lista_reais_final):
+    assert subtracao_de_listas(lista_numeros_inteiros, lista_numeros_reais) == lista_reais_final
     
 
