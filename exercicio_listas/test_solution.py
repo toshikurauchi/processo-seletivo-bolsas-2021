4281dfdb5a57f62945ef2ca0b455ca47385af641
Cleiltonss <90421421+Cleiltonss@users.noreply.github.com>
2021-09-28 12:58:47 -0300

    Update test_solution.py

--- exercicio_listas/test_solution.py
+++ exercicio_listas/test_solution.py
@@ -5,7 +5,7 @@ import pytest
 
 #verifica lista_numeros_impares
 
-@pytest.mark.parametrize("test_input,expected", [([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [0, 2, 4, 6, 8]), ([1, 3, 5, 7, 9])])
+@pytest.mark.parametrize("test_input, expected", [([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [0, 2, 4, 6, 8]), ([1, 3, 5, 7, 9])])
 
 def test_subtracao_de_listas(test_input, expected):
     assert subtracao_de_listas(test_input) == expected
